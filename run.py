4844883d8d365e193f54664af08ecda3f9051788
David Lin 林恒 <davidlin@ieinet.org>
2026-06-09 17:54:18 +0800

    v1-3

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 from backend.app import create_app
 from backend.models import db
+from sqlalchemy import inspect
 
 app = create_app()
 
 with app.app_context():
-    db.create_all(checkfirst=True)
+    inspector = inspect(db.engine)
+    if not inspector.has_table("users"):
+        db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
